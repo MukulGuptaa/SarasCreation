1df7c8a058819c469f4b1d69059e3ea6639c9752
Mukul Gupta <mukul.gupta@games24x7.com>
2026-01-13 02:16:38 +0530

    Revert "stopped get api"
    This reverts commit 42cd7ccaddb92626e78f932c380ff853fa6067a1.

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -39,7 +39,6 @@ async function getProduct(req, res, next){
 
 async function getProducts(req, res, next){
     try{
-        throw new Error("test error");
         const { page = 1, limit = 10 } = req.query;
         
         // Parse filters from query string
